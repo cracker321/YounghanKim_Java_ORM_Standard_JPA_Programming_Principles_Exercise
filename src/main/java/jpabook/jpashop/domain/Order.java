cfbc20efceebd1d0fd4cc6aec1cc1f4e7479bf50
yujong <yujong4170@gmail.com>
2023-01-02 14:55:40 +0900

    yujong

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -15,7 +15,11 @@ public class Order {
     private Long id;
 
     @Column(name = "MEMBER_ID")
-    private Long memberId;
+    private Long memberId; //그러나, 이 방식은 '객체지향스럽지 않다!'
+                           //이 방식은, '자바 객체 설계'를 'DB 테이블 설계'에 맞춘 방식이다.
+                           //왜냐하면, '테이블의 외래키를 자바 객체에 그대로 가져왔고',
+                           //         '객체 그래프 탐색이 불가능'하며,
+                           //         '참조가 없으므로 UML도 잘못됨'.
 
     private LocalDateTime orderDate;
 
