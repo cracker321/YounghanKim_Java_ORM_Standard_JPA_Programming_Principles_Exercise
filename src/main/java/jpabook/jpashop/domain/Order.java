9b9c29dc60c64e128047774e361fa9bc358640fd
yujong <yujong4170@gmail.com>
2023-01-04 14:14:13 +0900

    yujong

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -14,12 +14,23 @@ public class Order {
     @Column(name = "ORDER_ID", length = 10)
     private Long id;
 
-    @Column(name = "MEMBER_ID")
-    private Long memberId; //그러나, 이 방식은 '객체지향스럽지 않다!'
-                           //이 방식은, '자바 객체 설계'를 'DB 테이블 설계'에 맞춘 방식이다.
-                           //왜냐하면, '테이블의 외래키를 자바 객체에 그대로 가져왔고',
-                           //         '객체 그래프 탐색이 불가능'하며,
-                           //         '참조가 없으므로 UML도 잘못됨'.
+    //< 'Order 객체'와 'Member 객체' 간의 '연관관계 매핑' >
+    //일단, 가급적 '단방향 매핑'을 제대로 해둔다! 이것만으로 '개체 설계'는 많이 됨.
+    @ManyToOne //'주문(Order) 객체'와 '회원(Member) 객체'의 관계 = N : 1
+    @JoinColumn(name = "MEMBER_ID") //'Order 객체의 필드 member(= 'Member 객체의 필드 id')'는
+                                    //'테이블 MEMBER의 컬럼 MEMBER_ID'에 대응된다!
+    private Member member; //'Order 객체의 필드 member'. FK = 'Member 객체의 필드 id'. PK
+                           //'Order 객체'의 입장에서는, '어떤 Member가 주문했는지' '그 Member ID'에 대한 정보가 필요함.
+                           //즉, 'member ID'가 필요하고, 여기서의 '필드 member'가 바로 그 'member ID'임.
+
+
+//    @Column(name = "MEMBER_ID")
+//    private Long memberId; //그러나, 이 방식은 '객체지향스럽지 않다!'
+//                           //이 방식은, '자바 객체 설계'를 'DB 테이블 설계'에 맞춘 방식이다.
+//                           //왜냐하면, '테이블의 외래키를 자바 객체에 그대로 가져왔고',
+//                           //         '객체 그래프 탐색이 불가능'하며,
+//                           //         '참조가 없으므로 UML도 잘못됨'.
+
 
     private LocalDateTime orderDate;
 
@@ -27,35 +38,11 @@ public class Order {
     private OrderStatus status;
 
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
-    public Long getMemberId() {
-        return memberId;
-    }
 
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
-    }
 
-    public LocalDateTime getOrderDate() {
-        return orderDate;
-    }
 
-    public void setOrderDate(LocalDateTime orderDate) {
-        this.orderDate = orderDate;
-    }
 
-    public OrderStatus getStatus() {
-        return status;
-    }
 
-    public void setStatus(OrderStatus status) {
-        this.status = status;
-    }
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,8 +1,6 @@
 package jpabook.jpashop.domain;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 public class OrderItem {
 
@@ -11,53 +9,35 @@ public class OrderItem {
     @Column(name = "ORDER_ITEM_ID")
     private Long id;
 
-    @Column(name = "ORDER_ID")
-    private Long orderId;
 
-    @Column(name = "ITEM_ID")
-    private Long itemId;
+    //< 'OrderItem 객체'와 'Order 객체' 간의 '연관관계 매핑' >
+    @ManyToOne //'주문상품(OrderItem) 객체'와 '주문(Order) 객체'의 관계 = N : 1
+    @JoinColumn(name = "ORDER_ID") //'OrderItem 객체의 필드 member(= 'Member 객체의 필드 id')'는
+                                   //'테이블 ORDER의 컬럼 ORDER_ID'에 대응된다!
+    private Order order; //'OrderItem 객체의 필드 order'. FK = 'Order 객체의 필드 id'. PK
+                         //'OrderItem 객체'의 입장에서는, '어떤 Order ID가 상품을 주문했는지' '그 Order ID'에 대한 정보가 필요함.
+                         //즉, 'Order ID'가 필요하고, 여기서의 '필드 order'가 바로 그 'order ID'임.
+
+
+    //< 'OrderItem 객체'와 'Item 객체' 간의 '연관관계 매핑' >
+    @ManyToOne //'주문상품(OrderItem) 객체'와 '상품(Item) 객체'의 관계 = N : 1
+    @JoinColumn(name = "ITEM_ID") //'OrderItem 객체의 필드 item(= 'Item 객체의 필드 id)'는
+                                  //'테이블 ITEM의 컬럼 ITEM_ID'에 대응된다!
+    private Item item; //'OrderItem 객체의 필드 item'. FK = 'Item 객체의 필드 id'. PK
+                       //'OrderItem 객체'의 입장에서는, '어떤 Item ID가 주문되었는지' '그 Item ID'에 대한 정보가 필요함.
+                       //즉, 'Item ID'가 필요하고, 여기서의 '필드 item'이 바로 그 'item ID'임.
+
+
+//    @Column(name = "ORDER_ID")
+//    private Long orderId;
+//
+//    @Column(name = "ITEM_ID")
+//    private Long itemId;
 
     private int orderPrice;
 
     private int count; //구매수량
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(Long orderId) {
-        this.orderId = orderId;
-    }
-
-    public Long getItemId() {
-        return itemId;
-    }
-
-    public void setItemId(Long itemId) {
-        this.itemId = itemId;
-    }
-
-    public int getOrderPrice() {
-        return orderPrice;
-    }
-
-    public void setOrderPrice(int orderPrice) {
-        this.orderPrice = orderPrice;
-    }
-
-    public int getCount() {
-        return count;
-    }
-
-    public void setCount(int count) {
-        this.count = count;
     }
 }
