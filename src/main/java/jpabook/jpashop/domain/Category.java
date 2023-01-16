4f96869de2e0a9e2f52907bd7befb039b7b92964
yujong <yujong4170@gmail.com>
2023-01-16 19:46:01 +0900

    yujong

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -18,7 +18,7 @@ public class Category {
 
     //=========================================================================================================
     //< 셀프 연관관계 > : 웹페이지에서 카테고리가 아래로 쭉~ 내려가지는데, 그것을 셀프로 하는 것. 이것은 이 강의의 핵심이 아님
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "PARENT_ID")
     private Category parent; //상위 카테고리
 
--- src/main/java/jpabook/jpashop/domain/CategoryItem.java
+++ src/main/java/jpabook/jpashop/domain/CategoryItem.java
@@ -19,7 +19,7 @@ public class CategoryItem {
 
     //< 'CategoryItem 객체의 필드 category'와 'Category 객체의 필드 id(1)' 간의 'N : 1 단방향' 매핑 >
     //- '주인(N : M 일 때 기준으로)'이 'CategoryItem 객체의 필드 category(='Category 객체의 필드 id')'인 경우
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "CATEGORY_ID") //'현재 테이블 CATEGORY_ITEM의 FK인 필드 category(='Catgory 객체의 필드 id')'는
                                       //'주인이 아닌 테이블 CATEGORY의 PK인 컬럼 CATEGORY_ID'에 대응된다!
     private Category category; //='Cateogry 객체의 필드 id'
@@ -28,7 +28,7 @@ public class CategoryItem {
 
     //< 'CategoryItem 객체의 필드 item(N)'과 'Item 객체의 필드 id(1) 간의 'N : 1 단방향' 매핑 >
     //- '주인(N: M 일 때 기준으로)'이 'CategoryItem 객체의 필드 item(='Item 객체의 필드 id')'인 경우
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ITEM_ID") //'현재 테이블 CATEGORY_ITEM의 FK인 필드 item(='Item 객체의 필드 id')'은
                                   //'주인이 아닌 테이블 ITEM의 PK인 컬럼 ITEM_ID'에 대응된다!
     private Item item; //='Item 객체의 핃드 id'
